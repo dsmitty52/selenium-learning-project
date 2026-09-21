0a947211820ecc78035e2d9bfcf5560ac71385b4
dsmitty52 <dsmitty52@gmail.com>
2026-09-21 16:29:41 -0400

    Add GitHub Actions CI workflow with headless Chrome

--- conftest.py
+++ conftest.py
@@ -1,8 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+options = Options()
+
+if os.environ.get("CI") == "true":
+   options.add_argument("--headless=new")
 
 @pytest.fixture
 def driver():
-   d= webdriver.Chrome()
+   d= webdriver.Chrome(options=options)
    yield d
    d.quit()
